40e493b982b8eb9c0e47413d91a92b8db77a97cd
woooyoung <axdsw121@gmail.com>
2025-03-21 14:49:25 +0900

    리팩토링, 각각의 컨트롤러로 기능 분리

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.example.motivation.controller.MotivationController;
 import org.example.motivation.entity.Motivation;
+import org.example.system.controller.SystemController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,16 +19,14 @@ public class App {
     public void run() {
         System.out.println("== motivation 실행 ==");
 
-        int lastId = 0;
-
-        List<Motivation> motivations = new ArrayList<>();
+        SystemController systemController = new SystemController();
+        MotivationController motivationController = new MotivationController(sc);
 
         while (true) {
             System.out.print("명령어) ");
             String cmd = sc.nextLine().trim();
-
             if (cmd.equals("exit")) {
-                System.out.println("== motivation 종료 ==");
+                systemController.exit();
                 break;
             } else if (cmd.length() == 0) {
                 System.out.println("명령어가 입력되지 않았음");
@@ -34,38 +34,9 @@ public class App {
             }
 
             if (cmd.equals("add")) {
-                int id = lastId + 1;
-                System.out.print("body : ");
-                String body = sc.nextLine();
-                System.out.print("source : ");
-                String source = sc.nextLine();
-
-                Motivation motivation = new Motivation(id, body, source);
-
-                motivations.add(motivation);
-
-                System.out.printf("%d번 motivation이 등록됨\n", id);
-                lastId++;
+                motivationController.add();
             } else if (cmd.equals("list")) {
-                if (motivations.size() == 0) {
-                    System.out.println("등록된 moti 없어");
-                    continue;
-                }
-
-                System.out.println("=".repeat(40));
-                System.out.printf("   id    /     source      /      body        \n");
-
-                for (int i = motivations.size() - 1; i >= 0; i--) {
-                    Motivation motivation = motivations.get(i);
-
-                    if (motivation.getSource().length() > 7) {
-                        System.out.printf("   %d    /     %s     /      %s        \n", motivation.getId(), motivation.getSource().substring(0, 5) + "...", motivation.getBody());
-                        continue;
-                    }
-                    System.out.printf("   %d    /     %s        /      %s        \n", motivation.getId(), motivation.getSource(), motivation.getBody());
-                }
-
-                System.out.println("=".repeat(40));
+                motivationController.list();
             } else {
                 System.out.println("사용할 수 없는 명령어야");
                 continue;
--- src/main/java/org/example/motivation/controller/MotivationController.java
+++ src/main/java/org/example/motivation/controller/MotivationController.java
@@ -1,4 +1,56 @@
 package org.example.motivation.controller;
 
+import org.example.motivation.entity.Motivation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MotivationController {
+
+    int lastId = 0; // 몇 번까지 썼더라?
+    List<Motivation> motivations = new ArrayList<>(); // motivation 저장// 소
+    Scanner sc;
+
+    public MotivationController(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void add() {
+
+        int id = lastId + 1;
+        System.out.print("body : ");
+        String body = sc.nextLine();
+        System.out.print("source : ");
+        String source = sc.nextLine();
+
+        Motivation motivation = new Motivation(id, body, source);
+
+        motivations.add(motivation);
+
+        System.out.printf("%d번 motivation이 등록됨\n", id);
+        lastId++;
+    }
+
+    public void list() {
+        if (motivations.size() == 0) {
+            System.out.println("등록된 moti 없어");
+            return;
+        }
+
+        System.out.println("=".repeat(40));
+        System.out.printf("   id    /     source      /      body        \n");
+
+        for (int i = motivations.size() - 1; i >= 0; i--) {
+            Motivation motivation = motivations.get(i);
+
+            if (motivation.getSource().length() > 7) {
+                System.out.printf("   %d    /     %s     /      %s        \n", motivation.getId(), motivation.getSource().substring(0, 5) + "...", motivation.getBody());
+                continue;
+            }
+            System.out.printf("   %d    /     %s        /      %s        \n", motivation.getId(), motivation.getSource(), motivation.getBody());
+        }
+
+        System.out.println("=".repeat(40));
+    }
 }
