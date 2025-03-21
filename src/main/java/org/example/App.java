24e1e10989a63bbe05b4f6db0c1626eef70b420b
woooyoung <axdsw121@gmail.com>
2025-03-21 13:46:40 +0900

    list 입력시 갖고 있는 motivation의 정보가 나오도록, 글자 수 많을 경우 처리

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -17,13 +17,7 @@ public class App {
 
         int lastId = 0;
 
-//        Motivation motivation0 = null;
-//        Motivation motivation1 = null;
-//        Motivation motivation2 = null;
-
-//        Motivation[] motivations = new Motivation[2];
-
-        List<Motivation> motivations = new ArrayList<Motivation>();
+        List<Motivation> motivations = new ArrayList<>();
 
         while (true) {
             System.out.print("명령어) ");
@@ -45,54 +39,71 @@ public class App {
                 String source = sc.nextLine();
 
                 Motivation motivation = new Motivation(id, body, source);
-//                motivation.id = id;
-//                motivation.body = body;
-//                motivation.source = source;
 
                 motivations.add(motivation);
-
-//                if (motivation.id == 1) {
-//                    motivation0 = motivation;
-//                } else if (motivation.id == 2) {
-//                    motivation1 = motivation;
-//                } else if (motivation.id == 3) {
-//                    motivation2 = motivation;
-//                }
 
                 System.out.printf("%d번 motivation이 등록됨\n", id);
                 lastId++;
             } else if (cmd.equals("list")) {
-                System.out.println("=".repeat(40));
-                System.out.printf("   번호    /     source      /      motivation        \n");
-//                System.out.println(motivation0.toString());
-//                System.out.println(motivation1.toString());
-//                System.out.println(motivation2.toString());
-//                for (Motivation motivation : motivations) {
-//                    System.out.println(motivation.toString());
-//                }
-
                 if (motivations.size() == 0) {
                     System.out.println("등록된 moti 없어");
-                } else {
-                    System.out.println("1개 이상 있음");
+                    continue;
                 }
 
                 System.out.println("=".repeat(40));
+                System.out.printf("   id    /     source      /      body        \n");
+
+                for (int i = motivations.size() - 1; i >= 0; i--) {
+                    Motivation motivation = motivations.get(i);
+
+                    if (motivation.getSource().length() > 7) {
+                        System.out.printf("   %d    /     %s     /      %s        \n", motivation.getId(), motivation.getSource().substring(0, 5) + "...", motivation.getBody());
+                        continue;
+                    }
+                    System.out.printf("   %d    /     %s        /      %s        \n", motivation.getId(), motivation.getSource(), motivation.getBody());
+                }
+
+                System.out.println("=".repeat(40));
+            } else {
+                System.out.println("사용할 수 없는 명령어야");
+                continue;
             }
-
-
         }
     }
 }
 
 class Motivation {
-    int id;
-    String body;
-    String source;
+    private int id;
+    private String body;
+    private String source;
 
     public Motivation(int id, String body, String source) {
         this.id = id;
         this.body = body;
+        this.source = source;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
         this.source = source;
     }
 
