41901562afda9407492fa096e6b6ac0f378e99ff
seongho00 <tjdwns000927@gmail.com>
2025-03-21 13:13:37 +0900

    삭제, 삭제 커맨드 잘못 입력시, 없는 번호 삭제 시

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,9 @@
 package org.example;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,6 +21,7 @@ public class App {
         while (true) {
             System.out.print("명령어) ");
             String cmd = sc.nextLine().trim();
+
             if (cmd.isEmpty()) {
                 System.out.println("명령어가 입력되지 않았습니다.");
             }
@@ -26,6 +30,13 @@ public class App {
                 command.reg();
             } else if (cmd.equals("list")) {
                 command.list();
+            } else if (cmd.contains("remove")) {
+                try {
+                    List<String> a = new ArrayList<>(Arrays.asList(cmd.split(" ")));
+                    command.remove(Integer.parseInt(a.get(1)));
+                } catch (NumberFormatException e) {
+                    System.out.println("잘못된 명령어 입니다.");
+                }
             } else if (cmd.equals("exit")) {
                 command.end();
                 break;
@@ -70,20 +81,37 @@ class command {
     }
 
     static void list() {
-        System.out.println("==============");
-        System.out.println("번호 / source / motivation");
+        if (mot.isEmpty()) {
+            System.out.println();
 
-        for (int i = mot.size() - 1; i >= 0; i--) {
-            System.out.printf("%s / %s / %s\n", mot.get(i).index, mot.get(i).source, mot.get(i).motivation);
+        } else {
+            System.out.println("=".repeat(50));
+            System.out.println("번호 / source / motivation");
+
+            for (int i = mot.size() - 1; i >= 0; i--) {
+                System.out.printf("%d / %s / %s\n", mot.get(i).index, mot.get(i).source, mot.get(i).motivation);
+            }
+
+            System.out.println("=".repeat(50));
         }
 
-        System.out.println("==============");
     }
 
     static void end() {
         System.out.println("== motivation 종료 ==");
     }
 
+    static void remove(int number) {
+        try {
+            mot.remove(number - 1);
+            System.out.println(number + "번 motivation이 삭제되었습니다.");
+
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(number + "번 motivation은 존재하지 않습니다.");
+            
+        }
+
+    }
 }
 
 class mot_list extends command {
